3a88bdb4339354f3365538d64659d47bc9b22f7f
nicolecazulo <nicolecazulo@gmail.com>
2024-12-11 17:41:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,46 @@
-async function fetchOpenwhyd() {
-  let searchText = document.getElementById('musicSearch').value;
-  let url = 'https://openwhyd.org/search?context=addTrack&q=' + searchText;
+async function fetchLyricSuggestions() {  
+  let searchText = document.getElementById('lyricSearch').value;
+  
+  let apiUrl = 'https://api.lyrics.ovh';
+  let suggestUrl = apiUrl + '/suggest/' + searchText;
 
-  let libraryApiResponse = await fetch(url);
-  let resultsJson = await libraryApiResponse.json();
-  let tracks = resultsJson.docs;
+  let suggestApiResponse = await fetch(suggestUrl);
+  
+  let suggestJson = await suggestApiResponse.json();
+  let lyricSuggestions = suggestJson.data;
   
   let resultsTextContainer = document.getElementById("resultsText");  
-  resultsTextContainer.innerHTML = "<h3>Top 5 curated track results:</h3>";
+  resultsTextContainer.innerHTML = "<h3>Lyric results:</h3>";
   
-  let trackResultsContainer = document.getElementById("tracks");  
-  trackResultsContainer.innerHTML = "";
+  let lyricResultsContainer = document.getElementById("lyricSuggestions");  
+  lyricResultsContainer.innerHTML = "";
   
-  for(let i = 0; i < tracks.length; i++) {
-    let track = tracks[i];
-    let trackName = track.name;
-    let trackImage = track.img;
-    let trackPlatformUrl = track.url;
-    let trackUploader = track.uNm;
+  let lyricsContainer = document.getElementById("lyrics");  
+  lyricsContainer.innerHTML = "";
+  
+  for(let i = 0; i < lyricSuggestions.length; i++) {
+    let lyricSuggestion = lyricSuggestions[i];
+    let artist = lyricSuggestion.artist.name;
+    let album = lyricSuggestion.album.title;
+    let albumCover = lyricSuggestion.album.cover_big;
+    let title = lyricSuggestion.title;
     
-    if(trackImage.includes("ytimg.com") && trackImage.includes("/default.jpg")) {
-      let length = trackImage.length;
-      trackImage = trackImage.slice(0, length - 11) + 'sddefault.jpg';
-    }
+    let lyricUrl = apiUrl + '/v1/' + artist + '/' + title;
+
+    let lyricApiResponse = await fetch(lyricUrl);
+  
+    let lyricJson = await lyricApiResponse.json();
+    let lyrics = lyricJson.lyrics;
     
-    trackResultsContainer.innerHTML += `
-    <div class="col">
+    lyricResultsContainer.innerHTML += `
+    <div  class="col">
       <div class="card shadow-sm">
-        <img src="${trackImage}" width="100%" height="300"/>
+        <img id="lyric-${i}" src="${albumCover}" width="100%" height="300"/>
         <div class="card-body">
-            <p><strong>Track Name:</strong> ${trackName}</p>
-            <p><strong>Url:</strong> ${trackPlatformUrl}</p>
-            <p><strong>Track Uploader:</strong> ${trackUploader}</p>
+            <p><strong>Artist:</strong>${artist}</p>
+            <p><strong>Album:</strong>${album}</p>
+            <p><strong>Title:</strong> ${title}</p>
+            <p class="text-center">${lyrics}</p>
         </div>
       </div>
     </div>`;
